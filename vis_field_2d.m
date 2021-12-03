abd76fc0b0f5610ca0c92484150430a147e2ecd0
Capprin Bass <capprin@gmail.com>
2021-12-03 14:02:02 -0800

    Added critical point estimation for 2D fields

--- vis_field_2d.m
+++ vis_field_2d.m
@@ -30,8 +30,36 @@ function f = vis_field_2d(grid, field, method, levels)
             axis('on');
         case 'critical'
             % critical points
-            % actually requires bilinear interp. this is a pita so will
-            % come later
+            % using area of many contours to determine max/min
+            C = contourc(field, levels);
+            % build list of contours, compute areas
+            C_struct = get_contours(C);
+            areas = zeros(1,length(C_struct));
+            for i = 1:length(C_struct)
+                % only use closed polygons
+                if C_struct(i).x(1) == C_struct(i).x(end) &&...
+                   C_struct(i).y(1) == C_struct(i).y(end)
+                    areas(i) = polyarea(C_struct(i).x, C_struct(i).y);
+                else
+                    areas(i) = 0;
+                end
+                
+            end
+            % ignore lines (zero area)
+            used = areas~=0;
+            areas = areas(used);
+            C_struct = C_struct(used);
+            % get smallest contours, within some tolerance
+            tol = 1/(levels);
+            min_area = min(areas); 
+            point_idxs = find(areas < min_area + tol*min_area);
+            % get centerpoints
+            points = zeros(2,length(point_idxs));
+            for i = 1:length(points)
+                points(1,i) = mean(C_struct(point_idxs(i)).x);
+                points(2,i) = mean(C_struct(point_idxs(i)).y);
+            end
+            scatter(points(1,:), points(2,:), '.k');
     end
     % labels
     xlabel('\alpha_1');
