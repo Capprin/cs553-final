8258d1aef5c7d6b15dcd36f70a23ab12bb517eb6
Capprin Bass <capprin@gmail.com>
2021-12-02 16:17:52 -0800

    Wrote 2d field visualizations except critpoints

---
+++ vis_field_2d.m
@@ -0,0 +1,42 @@
+% vis_field_2d.m
+% selection of 2D scalar field visualizations
+% inputs:
+    % grid: ndgrid
+    % field: scalar field defined over grid
+    % method: visualization method; any of {contour, gradient, critical}
+
+function vis_field_2d(grid, field, method)
+    % figure setup
+    figure(69);
+    hold on;
+    % sanitize
+    if ~exist('method', 'var')
+        method = 'contour';
+    end
+    % visualization techniques
+    switch method
+        case 'contour'
+            % contour plot
+            contour(field);
+        case 'gradient'
+            % gradient plot (rotate to respect meshgrid)
+            image(1, 1, field, 'CdataMapping', 'scaled','Interpolation','bilinear');
+            axis('on');
+            colorbar;
+        case 'critical'
+            % critical points
+            % actually requires bilinear interp. this is a pita so will
+            % come later
+    end
+    % labels
+    xlabel('\alpha_1');
+    ylabel('\alpha_2');
+    title('2D Scalar Field Visualization');
+    xl = xlim;
+    xticks(linspace(xl(1), xl(2), 7));
+    xticklabels({'' '-2' '-1' '0' '1' '2' ''}); %lazy formatting, data known
+    yl = ylim;
+    yticks(linspace(yl(1), yl(2), 7));
+    yticklabels({'' '-2' '-1' '0' '1' '2' ''});
+    axis('equal')
+end
